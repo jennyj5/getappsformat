6d687d619d185a486fed4501c1cc4e6eca4a9626
jennyj5 <jennyj5@mit.edu>
2015-12-31 14:55:02 -0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,31 +2,29 @@ import scraperwiki
 from scrapemark import scrape
 import scraperwiki
 count = 0
-for page in range(1,40):
+
+FormatList = ["Managed","Native", "Mobile", "iPad", "iPhone","Android", "Salesforce1","Lightning Ready"]
+FilterList = ["9","6","10","11","12","13","14","15"]
+
+for Filter0 in FilterList:
+  print count
+  for page in range(1,40):
+    URLPath = URLRoot.format(str(page))
+    URL = URLPath+Filter0
+    print URL
+  
+    html = scraperwiki.scrape(URL)
+    scrape_data = scrape("""
+     {*
+     
+     <a class="tile-title" href="{{ [mobile].[link] }}" id="{{ [mobile].[id] }}" title="{{ [mobile].[title1] }}"></a>
+     
+     *}
+     """, html=html);
+  
+    data = [{'Title':p['title1'][0], 'URL':p['link'][0], 'ID':p['id'][0], 'Format':FormatList[count]} for p in scrape_data['mobile']]
+  
+    scraperwiki.sqlite.save(unique_keys=["URL"], data=data)
   count = count+1
   if count>2:
     break
-  print count
-  URL = "https://appexchange.salesforce.com/results?pageNo="+str(page)+"&filter=9" #Managed
-  #URL = "https://appexchange.salesforce.com/results?pageNo="+str(page)+"&filter=a0L3000000OvSOFEA3" #Customer Service
-  #URL = "https://appexchange.salesforce.com/results?pageNo="+str(page)+"&filter=a0L3000000OvSOGEA3" #Marketing
-  #URL = "https://appexchange.salesforce.com/results?pageNo="+str(page)+"&filter=a0L3000000OvSOHEA3" #IT & Admin
-  #URL = "https://appexchange.salesforce.com/results?pageNo="+str(page)+"&filter=a0L3000000OvSOIEA3" #HR
-  #URL = "https://appexchange.salesforce.com/results?pageNo="+str(page)+"&filter=a0L3000000OvSOJEA3" #Finance
-  #URL = "https://appexchange.salesforce.com/results?pageNo="+str(page)+"&filter=a0L3000000OvSOKEA3" #ERP
-  #URL = "https://appexchange.salesforce.com/results?pageNo="+str(page)+"&filter=a0L3000000OvSOLEA3" #Collabration
-  #URL = "https://appexchange.salesforce.com/category/analytics" #Analytics
-  
-  
-  html = scraperwiki.scrape(URL)
-  scrape_data = scrape("""
-   {*
-   
-   <a class="tile-title" href="{{ [mobile].[link] }}" id="{{ [mobile].[id] }}" title="{{ [mobile].[title1] }}"></a>
-   
-   *}
-   """, html=html);
-
-  data = [{'Title':p['title1'][0], 'URL':p['link'][0], 'ID':p['id'][0], 'Format':'Managed'} for p in scrape_data['mobile']]
-
-  scraperwiki.sqlite.save(unique_keys=["URL"], data=data)
